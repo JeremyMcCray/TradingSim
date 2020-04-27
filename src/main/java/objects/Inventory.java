fc7cfa021828c96cc639734d5c0837ec43c261ac
Sam <claysamuelc@gmail.com>
2020-04-27 01:35:31 -0500

    first commit

--- src/main/java/objects/Inventory.java
+++ src/main/java/objects/Inventory.java
@@ -1,17 +1,17 @@
 package objects;
 
 public class Inventory {
-    int amountOfgems;
-    int amountOfgold;
-    int amountOfiron;
-    int amountOfwood;
-    int amountOfwheat;
+    int numGems;
+    int numGold;
+    int numIron;
+    int numWood;
+    int numWheat;
 
     public Inventory(int gems, int iron, int wood, int wheat, int gold) {
-        this.amountOfgems = gems;
-        this.amountOfgold = gold;
-        this.amountOfiron = iron;
-        this.amountOfwood = wood;
-        this.amountOfwheat = wheat;
+        this.numGems = gems;
+        this.numGold = gold;
+        this.numIron = iron;
+        this.numWood = wood;
+        this.numWheat = wheat;
     }
 }
