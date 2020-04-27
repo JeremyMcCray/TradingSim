cecdcdf27ec1bdd31e1878900e1d9deeb4bbd3bc
Sam <claysamuelc@gmail.com>
2020-04-27 01:25:59 -0500

    gold in inventory

--- src/main/java/objects/Inventory.java
+++ src/main/java/objects/Inventory.java
@@ -2,12 +2,14 @@ package objects;
 
 public class Inventory {
     int gems;
+    int gold;
     int iron;
     int wood;
     int wheat;
 
     public Inventory(int gems, int iron, int wood, int wheat) {
         this.gems = gems;
+        this.gold = gold;
         this.iron = iron;
         this.wood = wood;
         this.wheat = wheat;
