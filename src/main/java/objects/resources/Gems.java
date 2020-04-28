97dbe4cc4da4b332e3fb93528cd141eded10cb21
Sam <claysamuelc@gmail.com>
2020-04-28 16:02:21 -0500

    gems baseCost to Double

--- src/main/java/objects/resources/Gems.java
+++ src/main/java/objects/resources/Gems.java
@@ -3,7 +3,7 @@ package objects.resources;
 import objects.abstracts.AbstractResource;
 
 public class Gems extends AbstractResource {
-    public Gems(String name, String description, Integer baseCost, Integer size) {
+    public Gems(String name, String description, Double baseCost, Integer size) {
         super("gems", description, baseCost, size);
     }
 }
